64c21335a717ecbb14a6ad1660b88c2e7b5ee1a1
Avrumi Friedman <a.friedman07@gmail.com>
2021-06-16 21:45:18 -0400

    Add project files.

---
+++ BlazorWASMDemo/Client/Pages/Counter.razor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorWASMDemo.Client.Pages
+{
+    public partial class Counter
+    {
+        private int currentCount = 0;
+        private int _incrementAmount = 0;
+
+        private string _text;
+
+        private void IncrementCount()
+        {
+            currentCount += _incrementAmount;
+        }
+    }
+}
